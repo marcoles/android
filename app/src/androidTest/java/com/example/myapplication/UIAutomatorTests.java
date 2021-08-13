505ec6ea357a0fb288aa4a7beb3901c1e139c60d
marcoles <oles.marcn@gmail.com>
2021-08-13 11:57:16 +0200

    update

--- app/src/androidTest/java/com/example/myapplication/UIAutomatorTests.java
+++ app/src/androidTest/java/com/example/myapplication/UIAutomatorTests.java
@@ -1,19 +1,16 @@
 package com.example.myapplication;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.filters.SdkSuppress;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
@@ -21,18 +18,16 @@ import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-
 @RunWith(AndroidJUnit4.class)
 public class UIAutomatorTests {
 
     private static final String BASIC_SAMPLE_PACKAGE = "com.example.myapplication";
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     private static final int LAUNCH_TIMEOUT = 5000;
     private static final String DEFAULT_MESSAGE = "Enter a message";
     private static final String CUSTOM_MESSAGE = "Hello!";
+    private Context context;
+    private UiObject2 button;
     private UiDevice device;
 
     @Before
@@ -50,7 +45,7 @@ public class UIAutomatorTests {
                 LAUNCH_TIMEOUT);
 
         // Launch the app
-        Context context = ApplicationProvider.getApplicationContext();
+        context = ApplicationProvider.getApplicationContext();
         final Intent intent = context.getPackageManager()
                 .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
         // Clear out any previous instances
@@ -60,11 +55,13 @@ public class UIAutomatorTests {
         // Wait for the app to appear
         device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
                 LAUNCH_TIMEOUT);
+
+        button = device.findObject(By.res(BASIC_SAMPLE_PACKAGE, "button"));
     }
 
     @Test
     public void clickTheButtonWithDefaultMessage() {
-        device.findObject(By.res(BASIC_SAMPLE_PACKAGE, "button")).click();
+        button.click();
         String displayedText =
                 device.wait(Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "textView")),
                         500).getText();
@@ -76,7 +73,7 @@ public class UIAutomatorTests {
     public void clickTheButtonWithCustomMessage() {
         device.findObject(By.res(
                 BASIC_SAMPLE_PACKAGE, "editTextTextPersonName")).setText(CUSTOM_MESSAGE);
-        device.findObject(By.res(BASIC_SAMPLE_PACKAGE, "button")).click();
+        button.click();
         String displayedText =
                 device.wait(Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "textView")),
                         500).getText();
@@ -86,15 +83,15 @@ public class UIAutomatorTests {
 
     @Test
     public void getToTheSecondScreen() {
-        Context context = ApplicationProvider.getApplicationContext();
         Intent intent = new Intent(context, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, CUSTOM_MESSAGE);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
         String displayedText =
                 device.wait(Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "textView")),
                         500).getText();
         assertThat("Displayed text does not match the custom message",
-                displayedText, is(equalTo("TextView")));
+                displayedText, is(equalTo(CUSTOM_MESSAGE)));
 
     }
 }
